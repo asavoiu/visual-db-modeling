87a5e1e99935e6dc74abf01e35daf8b2119ff33a
aurel <aurel.savoiu@cti.pub.ro>
2013-06-26 00:06:13 +0300

    made Api calls static
    Signed-off-by: aurel <aurel.savoiu@cti.pub.ro>

--- Project/Connectify/src/ro/visualDB/api/Api.java
+++ Project/Connectify/src/ro/visualDB/api/Api.java
@@ -9,24 +9,24 @@ import ro.visualDB.xml.XMLWriter;
 public class Api {
 
 	
-	public void exportToXML(TreeNode rootTn, String xmlFile) throws Exception {
+	public static void exportToXML(TreeNode rootTn, String xmlFile) throws Exception {
 		XMLWriter.writeToFile(xmlFile, rootTn);			
 	}
 	
-	public TreeNode importFromXML(String xmlFile) throws Exception {
+	public static TreeNode importFromXML(String xmlFile) throws Exception {
 		return XMLReader.readFromFile(xmlFile);
 	}
 	
-	public TreeNode importFromRemote(Remote rmt) throws Exception {
+	public static TreeNode importFromRemote(Remote rmt) throws Exception {
 		DBInfoProcessor dbip = new DBInfoProcessor(rmt.getConnection());						
 		return dbip.buildTreeForRemoteConnectionTreeNode(rmt);
 	}
 	
-	public String getCreateSQLScripts(TreeNode treeNode, int engine) throws Exception {
+	public static String getCreateSQLScripts(TreeNode treeNode, int engine) throws Exception {
 		return treeNode.getSqlStatement(engine);
 	}
 	
-	public String getModifySqlStatement(TreeNode treeNode, int engine) throws Exception {
+	public static String getModifySqlStatement(TreeNode treeNode, int engine) throws Exception {
 		return treeNode.getModifySqlStatement(engine);
 	}
 }
