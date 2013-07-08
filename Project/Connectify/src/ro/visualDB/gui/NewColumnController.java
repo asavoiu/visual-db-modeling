5bb8c64f9b0a1bbdaf535a62ba1fce9bd0ebd73e
Bogdan Grama <grama.bogdan90@gmail.com>
2013-07-09 02:22:47 +0300

    New column controller

---
+++ Project/Connectify/src/ro/visualDB/gui/NewColumnController.java
@@ -0,0 +1,11 @@
+package ro.visualDB.gui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Bogdan
+ * Date: 09.07.2013
+ * Time: 02:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewColumnController {
+}
